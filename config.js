bd2d8b02d81057202601d846ab0d3300d99c7b55
sevskii <sevskii111@gmail.com>
2019-11-05 16:54:57 +0300

    game 3

--- config.js
+++ config.js
@@ -2,42 +2,70 @@ module.exports = {
     users: [{
             admin: true,
             username: "admin",
-            password: "admin2"
+            password: "admin3"
         },
         {
-            color: "#8BD2D8",
+            color: "#e6194B",
             username: "1",
             password: "naruto"
         },
         {
-            color: "#C6A68F",
+            color: "#f58231",
             username: "2",
             password: "jojo"
         },
         {
-            color: "#F15E3D",
+            color: "#ffe119",
             username: "3",
             password: "miku"
         }, {
-            color: "#CC3B7C",
+            color: "#bfef45",
             username: "4",
             password: "saitama"
         }, {
-            color: "#56704B",
+            color: "#3cb44b",
             username: "5",
             password: "rem"
         }, {
-            color: "#EFCB5D",
+            color: "#42d4f4",
             username: "6",
             password: "anime"
         }, {
-            color: "#EFCB5D",
+            color: "#000075",
             username: "7",
             password: "liza"
         }, {
-            color: "#795446",
+            color: "#911eb4",
             username: "8",
             password: "monika"
+        }, {
+            color: "#f032e6",
+            username: "9",
+            password: "ooshio"
+        }, {
+            color: "#800000",
+            username: "10",
+            password: "reigen"
+        }, {
+            color: "#808000",
+            username: "11",
+            password: "hosaka"
+        }, {
+            color: "#aaffc3",
+            username: "12",
+            password: "toyokawa"
+        }, {
+            color: "#000000",
+            username: "13",
+            password: "asahi"
+        }, {
+            color: "#469990",
+            username: "14",
+            password: "aoki"
+        }, {
+            color: "#fabebe",
+            username: "15",
+            password: "sakurai"
         }
     ],
     timeout: 3000,
